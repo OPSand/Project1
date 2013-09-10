a459b160c48a68423ae90c91a478559a94c85c03
NathalieB <nbonatout@gmail.com>
2013-09-10 08:58:09 +0200

    (+ matching stdafx.h)

--- Lab1/Lab1/stdafx.h
+++ Lab1/Lab1/stdafx.h
@@ -16,7 +16,36 @@
 #define TYPE double
 //#define EXA
 //#define EXB
+//#define EXC
+//#define EXD
 typedef arma::vec arr; // it's good to be a pirate! (this is our untyped array/vector type)
 typedef arma::mat matr; // matrix class (now untyped)
+
+// Preprocessing instruction ...
+/*template<typename T1>
+extern "C" bool lu
+  (
+         Mat<typename T1::elem_type>&    L,
+         Mat<typename T1::elem_type>&    U,
+  const Base<typename T1::elem_type,T1>& X,
+  const typename arma_blas_type_only<typename T1::elem_type>::result* junk = 0
+  )
+  {
+  arma_extra_debug_sigprint();
+  arma_ignore(junk);
+  
+  arma_debug_check( (&L == &U), "lu(): L and U are the same object");
+  
+  const bool status = auxlib::lu(L, U, X);
+  
+  if(status == false)
+    {
+    L.reset();
+    U.reset();
+    arma_bad("lu(): failed to converge", false);
+    }
+  
+  return status;
+  };*/
 
 // TODO: faites référence ici aux en-têtes supplémentaires nécessaires au programme
